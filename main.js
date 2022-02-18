7dc30f78bfd12db9897eff133e6d37fc69e2281c
Joseph Maifeld <josephmaifeld@gmail.com>
2022-02-18 11:13:10 -0600

    bdays

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-console.log("hello");
+const myInitials = () => {
+    const bDays = { 
+       Alex: "June 1st 1989"
+       Javontae: "Febraury 28th 2002"
+       Joe: "June 22nd 1987"
+    }
+   return bDay
+ }
+
+ console.log()
+
+// teammate4 ( if needed ) : `myInitials-log-team-pets`
+// Everyone works on changes
+// teammate1: Adds CSS for the background, and sets a font color for all H1 tags, sets a global font on the body
+// teammate2: Writes a function that console.logs everyone's names, one name per line
+// teammate3: Writes a function that console.logs everyone's birthdays, one name per line
+// teammate 4 (if needed ) : writes a function that console.logs everyone's pets. If they don't have a pet, log out that that person is sad and lonely
+// Each teammate adds and commits their changes.
+// Do the following ONE teammate at a time:
+// A teammate pushes up their branch ( NOT to main! ) and creates a PR with a message that describes the changes and/or additions and the filenames that were changed
+// Another teammate will be responsible for reviewing that PR.
+// Make sure PR has a clear and descriptive message that helps you know what files were affected
+// make sure you've committed your work on your branch
+// pull down the remote branches with git fetch --all
+// Check that it works properly in your browser
+// If all is well, add a +1 ( thumbs-up ) message to the PR
+// DO NOT merge the PR
+// Teammate whose PR was given the thumbs-up merges their code and deletes their GH branch afterward
+// All teammates update their local main by checking out main and pulling down the remote main
+// All teammates switch back to their branch
+// All teammates merge main into their branch
+// Any teammates with merge conflicts fix the conflicts
+// Go to VS Code and view file(s) where conflicts occurred
+// Choose the version you want to keep ( the incoming code from main or your code on your branch )
+// Go back to the terminal and type git status you will see both modified meaning two versions of the same file were updated. This is expected and good. Add file(s) and commit with message "fixed merge conflict"
+// After merge and/or fixed merge conflict, push up your code again ( yes, you can push a branch multiple times )
+// Repeat this process for each teammate until all PRs are reviewed, approved, merged and each teammate has their local main branch up to date with the latest code
+// Call in an instructor to review your work and to ask any lingering questions about the git workflow
+// Take a break and celebrate having completed a really long, tough process that you will repeat about a million times while you're here!
